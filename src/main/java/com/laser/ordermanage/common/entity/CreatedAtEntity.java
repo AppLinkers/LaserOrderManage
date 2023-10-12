588dd13ff286ab49c319130aa8e5255a39dce9af
yuseogi0218 <yuseogi0218@gmail.com>
2023-10-12 14:58:29 +0900

    FEAT - domain order

---
+++ src/main/java/com/laser/ordermanage/common/entity/CreatedAtEntity.java
@@ -0,0 +1,21 @@
+package com.laser.ordermanage.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class CreatedAtEntity {
+
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdAt;
+}
--- src/main/java/com/laser/ordermanage/user/domain/UserEntity.java
+++ src/main/java/com/laser/ordermanage/user/domain/UserEntity.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user_table")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class UserEntity implements UserDetails {

0d4431415b70f01fe234aeff17d5586ead0b0a04
yuseogi0218 <yuseogi0218@gmail.com>
2023-10-31 11:10:22 +0900

    FEAT - cors setting

--- src/main/java/com/laser/ordermanage/user/controller/AuthController.java
+++ src/main/java/com/laser/ordermanage/user/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.laser.ordermanage.user.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -54,7 +55,7 @@ public class AuthController {
                 .maxAge(ExpireTime.REFRESH_TOKEN_EXPIRE_TIME_FOR_REDIS_AND_COOKIE)
                 .path("/") // 모든 곳에서 쿠키열람이 가능하도록 설정
                 .secure(false) // true : https 환경에서만 쿠키가 발동합니다.
-                .sameSite("None") // 동일 사이트과 크로스 사이트에 모두 쿠키 전송이 가능합니다.
+                .sameSite(Cookie.SameSite.NONE.attributeValue()) // 동일 사이트과 크로스 사이트에 모두 쿠키 전송이 가능합니다.
                 .httpOnly(true) // 브라우저에서 쿠키에 접근할 수 없도록 제한
                 .build();
     }
@@ -64,7 +65,7 @@ public class AuthController {
                 .maxAge(ExpireTime.REFRESH_TOKEN_EXPIRE_TIME_FOR_REDIS_AND_COOKIE)
                 .path("/") // 모든 곳에서 쿠키열람이 가능하도록 설정
                 .secure(false) // true : https 환경에서만 쿠키가 발동합니다.
-                .sameSite("None") // 동일 사이트과 크로스 사이트에 모두 쿠키 전송이 가능합니다.
+                .sameSite(Cookie.SameSite.NONE.attributeValue()) // 동일 사이트과 크로스 사이트에 모두 쿠키 전송이 가능합니다.
                 .httpOnly(true) // 브라우저에서 쿠키에 접근할 수 없도록 제한
                 .build();
     }

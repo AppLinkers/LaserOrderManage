cbdcee1795b9177a5b6ca378e51d0fe939dbe6a9
yuseogi0218 <yuseogi0218@gmail.com>
2023-10-31 15:38:14 +0900

    TEST - re-issue

--- src/main/java/com/laser/ordermanage/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laser/ordermanage/common/exception/GlobalExceptionHandler.java
@@ -55,6 +55,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MissingRequestCookieException.class)
     public ResponseEntity<?> missingRequestCookieException(MissingRequestCookieException e) {
+        e.printStackTrace();
         CustomCommonException exception = new CustomCommonException(ErrorCode.MISSING_JWT_TOKEN);
         return ResponseEntity.status(exception.getHttpStatus()).body(exception.toErrorResponse());
     }

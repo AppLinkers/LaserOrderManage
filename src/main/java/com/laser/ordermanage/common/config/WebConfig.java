9c98c0f7bdb0819a504ee5f55d5e2be8c4fcf56b
YooSeok Lee <64399505+yuseogi0218@users.noreply.github.com>
2023-12-01 10:42:10 +0900

    FIX - delete @EnableWebMvc
    LocalDate response List --> String

--- src/main/java/com/laser/ordermanage/common/config/WebConfig.java
+++ src/main/java/com/laser/ordermanage/common/config/WebConfig.java
@@ -3,10 +3,8 @@ package com.laser.ordermanage.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 

a0fbb71ebd1b2b45206225a8586ab174d61f9abd
yuseogi0218 <yuseogi0218@gmail.com>
2023-12-14 10:54:42 +0900

    #97 FEAT - 거래 긴급 설정 시 메일 전송 기능

--- src/main/java/com/laser/ordermanage/factory/service/FactoryOrderService.java
+++ src/main/java/com/laser/ordermanage/factory/service/FactoryOrderService.java
@@ -2,6 +2,7 @@ package com.laser.ordermanage.factory.service;
 
 import com.laser.ordermanage.common.exception.CustomCommonException;
 import com.laser.ordermanage.common.exception.ErrorCode;
+import com.laser.ordermanage.common.mail.MailService;
 import com.laser.ordermanage.factory.dto.request.FactoryUpdateOrderIsUrgentRequest;
 import com.laser.ordermanage.order.domain.Order;
 import com.laser.ordermanage.order.service.OrderService;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class FactoryOrderService {
 
     private final OrderService orderService;
+    private final MailService mailService;
 
     @Transactional
     public void updateOrderIsUrgent(Long orderId, FactoryUpdateOrderIsUrgentRequest request) {
@@ -24,6 +26,34 @@ public class FactoryOrderService {
         }
 
         order.updateIsUrgent(request.getIsUrgent());
+
+        String toEmail = order.getCustomer().getUser().getEmail();
+
+        StringBuilder sbTitle = new StringBuilder();
+        StringBuilder sbContent = new StringBuilder();
+
+        if (request.getIsUrgent()) {
+            sbTitle.append("[거래 긴급 설정] ")
+                    .append(order.getName())
+                    .append(" 거래의 긴급이 설정 되었습니다.");
+
+            sbContent.append("고객님, ")
+                    .append(order.getName())
+                    .append(" 거래의 긴급이 설정 되었습니다.");
+        } else {
+            sbTitle.append("[거래 긴급 설정 해제] ")
+                    .append(order.getName())
+                    .append(" 거래의 긴급 설정이 해제 되었습니다.");
+
+            sbContent.append("고객님, ")
+                    .append(order.getName())
+                    .append(" 거래의 긴급 설정이 해제 되었습니다.");
+        }
+
+        String title = sbTitle.toString();
+        String content = sbContent.toString();
+
+        mailService.sendEmail(toEmail, title, content);
     }
 
 }

2474f1acc9b211f53ac4edd91b6254bc13cd4903
yuseogi0218 <yuseogi0218@gmail.com>
2023-12-18 10:15:38 +0900

    FIX - 댓글 작성 시, 댓글 내용의 정규 표현식에 공백 문자열 포함

--- src/main/java/com/laser/ordermanage/order/dto/request/CreateCommentRequest.java
+++ src/main/java/com/laser/ordermanage/order/dto/request/CreateCommentRequest.java
@@ -8,6 +8,6 @@ import lombok.Getter;
 public class CreateCommentRequest {
 
     @NotEmpty(message = "댓글 내용은 필수 입력값입니다.")
-    @Pattern(regexp = "^.{0,200}$", message = "댓글 내용의 최대 글자수는 200자입니다.")
+    @Pattern(regexp = "^[\\s\\S]{0,200}$", message = "댓글 내용의 최대 글자수는 200자입니다.")
     private String content;
 }

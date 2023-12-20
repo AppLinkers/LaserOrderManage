a905bb6b01465ccf99a854478b7d3c007e6efe22
yuseogi0218 <yuseogi0218@gmail.com>
2023-12-20 15:02:17 +0900

    #121 FEAT - 비밀번호 찾기 - 이메일 인증 코드 요청

--- src/main/java/com/laser/ordermanage/user/api/UserAccountAPI.java
+++ src/main/java/com/laser/ordermanage/user/api/UserAccountAPI.java
@@ -3,12 +3,11 @@ package com.laser.ordermanage.user.api;
 import com.laser.ordermanage.user.dto.request.GetUserEmailRequest;
 import com.laser.ordermanage.user.service.UserAccountService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -24,5 +23,24 @@ public class UserAccountAPI {
     @GetMapping("/email")
     public ResponseEntity<?> getUserEmail(@RequestBody @Valid GetUserEmailRequest request) {
         return ResponseEntity.ok(userAccountService.getUserEmail(request));
+    }
+
+    /**
+     * 비밀번호 찾기 - 이메일 인증코드 요청
+     * - 이메일 기준으로 사용자 조회
+     * - 이메일 인증 코드 생성
+     * - 해당 사용자의 이메일로 인증코드 전송
+     * - Redis Verify Code 에 인증 코드 데이터 저장
+     */
+    @PostMapping("/password/request-verify")
+    public ResponseEntity<?> requestEmailVerifyForGetPassword(
+            @NotEmpty(message = "이메일은 필수 입력값입니다.")
+            @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+            @RequestParam(value = "email") String email
+    ) {
+
+        userAccountService.requestEmailVerifyForGetPassword(email);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/laser/ordermanage/user/service/UserAccountService.java
+++ src/main/java/com/laser/ordermanage/user/service/UserAccountService.java
@@ -1,6 +1,10 @@
 package com.laser.ordermanage.user.service;
 
+import com.laser.ordermanage.common.cache.redis.dao.VerifyCode;
+import com.laser.ordermanage.common.cache.redis.repository.VerifyCodeRedisRepository;
+import com.laser.ordermanage.common.mail.MailService;
 import com.laser.ordermanage.common.paging.ListResponse;
+import com.laser.ordermanage.user.domain.UserEntity;
 import com.laser.ordermanage.user.dto.request.GetUserEmailRequest;
 import com.laser.ordermanage.user.dto.response.GetUserEmailResponse;
 import com.laser.ordermanage.user.repository.UserEntityRepository;
@@ -12,11 +16,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserAccountService {
 
+    private final MailService mailService;
+    private final UserAuthService userAuthService;
+
     private final UserEntityRepository userRepository;
+    private final VerifyCodeRedisRepository verifyCodeRedisRepository;
 
     @Transactional(readOnly = true)
     public ListResponse<GetUserEmailResponse> getUserEmail(GetUserEmailRequest request) {
         return new ListResponse<>(userRepository.findEmailByNameAndPhone(request.getName(), request.getPhone()));
     }
 
+    @Transactional
+    public void requestEmailVerifyForGetPassword(String email) {
+
+        UserEntity user = userAuthService.getUserByEmail(email);
+
+        String title = "금오 M.T 비밀번호 찾기 이메일 인증 번호";
+        String verifyCode = mailService.createVerifyCode();
+        mailService.sendEmail(user.getEmail(), title, verifyCode);
+        // 이메일 인증번호 Redis 에 저장
+        verifyCodeRedisRepository.save(
+                VerifyCode.builder()
+                        .email(user.getEmail())
+                        .code(verifyCode)
+                        .build()
+        );
+    }
 }

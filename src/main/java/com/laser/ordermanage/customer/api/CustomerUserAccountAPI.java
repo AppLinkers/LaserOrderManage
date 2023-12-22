182aba014134e33f5cabd00760dbf3a7f5250b76
yuseogi0218 <yuseogi0218@gmail.com>
2023-12-22 10:58:50 +0900

    #124 FEAT - 고객의 마이페이지 - 계정 기본 정보 변경

--- src/main/java/com/laser/ordermanage/customer/api/CustomerUserAccountAPI.java
+++ src/main/java/com/laser/ordermanage/customer/api/CustomerUserAccountAPI.java
@@ -1,13 +1,13 @@
 package com.laser.ordermanage.customer.api;
 
+import com.laser.ordermanage.customer.dto.request.CustomerUpdateUserAccountRequest;
 import com.laser.ordermanage.customer.service.CustomerUserAccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/customer/user")
@@ -25,5 +25,20 @@ public class CustomerUserAccountAPI {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         return ResponseEntity.ok(customerUserAccountService.getUserAccount(user.getUsername()));
+    }
+
+    /**
+     * 고객의 마이페이지 - 계정 기본 정보 변경
+     * - 고객 회원의 이메일 기준으로 사용자 조회
+     * - 요청 데이터에 맞게 사용자 계정 정보 변경
+     */
+    @PatchMapping("")
+    public ResponseEntity<?> updateUserAccount(@RequestBody @Valid CustomerUpdateUserAccountRequest request) {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        customerUserAccountService.updateUserAccount(user.getUsername(), request);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/laser/ordermanage/customer/service/CustomerUserAccountService.java
+++ src/main/java/com/laser/ordermanage/customer/service/CustomerUserAccountService.java
@@ -1,6 +1,9 @@
 package com.laser.ordermanage.customer.service;
 
+import com.laser.ordermanage.customer.domain.Customer;
+import com.laser.ordermanage.customer.dto.request.CustomerUpdateUserAccountRequest;
 import com.laser.ordermanage.customer.dto.response.CustomerGetUserAccountResponse;
+import com.laser.ordermanage.customer.repository.CustomerRepository;
 import com.laser.ordermanage.user.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CustomerUserAccountService {
 
+    private final CustomerRepository customerRepository;
     private final UserEntityRepository userRepository;
 
     @Transactional(readOnly = true)
     public CustomerGetUserAccountResponse getUserAccount(String email) {
         return userRepository.findUserAccountByCustomer(email);
+    }
+
+    @Transactional
+    public void updateUserAccount(String email, CustomerUpdateUserAccountRequest request) {
+
+        Customer customer = customerRepository.findFirstByUserEmail(email);
+
+        customer.updateProperties(request);
     }
 }

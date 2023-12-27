28f2c09d8dcbf8ecedefb6878c859d5394697463
yuseogi0218 <yuseogi0218@gmail.com>
2023-12-27 10:20:32 +0900

    #137 TEST - 로그인 기능 API 단위 테스트

--- src/test/java/com/laser/ordermanage/common/APIUnitTest.java
+++ src/test/java/com/laser/ordermanage/common/APIUnitTest.java
@@ -6,6 +6,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 @ActiveProfiles("test")
 public class APIUnitTest {
 
--- src/test/java/com/laser/ordermanage/user/unit/api/UserAuthAPIUnitTest.java
+++ src/test/java/com/laser/ordermanage/user/unit/api/UserAuthAPIUnitTest.java
@@ -2,6 +2,8 @@ package com.laser.ordermanage.user.unit.api;
 
 import com.laser.ordermanage.common.APIUnitTest;
 import com.laser.ordermanage.common.constants.ExpireTime;
+import com.laser.ordermanage.common.exception.CustomCommonException;
+import com.laser.ordermanage.common.exception.ErrorCode;
 import com.laser.ordermanage.user.api.UserAuthAPI;
 import com.laser.ordermanage.user.domain.type.Role;
 import com.laser.ordermanage.user.dto.request.LoginRequest;
@@ -65,6 +67,124 @@ public class UserAuthAPIUnitTest extends APIUnitTest {
                 .andExpect(jsonPath("refreshToken").exists())
                 .andExpect(jsonPath("accessTokenExpirationTime").value(ExpireTime.ACCESS_TOKEN_EXPIRE_TIME))
                 .andExpect(jsonPath("refreshTokenExpirationTime").value(ExpireTime.REFRESH_TOKEN_EXPIRE_TIME));
+    }
+
+    /**
+     * 사용자 로그인 실패
+     * - 실패 사유 : 이메일 필드 - null
+     */
+    @Test
+    public void 로그인_실패_이메일_필드_null() throws Exception {
+        // given
+        final LoginRequest request = LoginRequest.builder()
+                .password("user1-password")
+                .build();
+
+        // when
+        final ResultActions resultActions = requestLogin(request);
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("httpStatus").value(ErrorCode.INVALID_FIELDS.getHttpStatus().name()))
+                .andExpect(jsonPath("errorCode").value(ErrorCode.INVALID_FIELDS.getCode()))
+                .andExpect(jsonPath("message").value("이메일은 필수 입력값입니다."));
+    }
+
+    /**
+     * 사용자 로그인 실패
+     * - 실패 사유 : 이메일 필드 유효성
+     */
+    @Test
+    public void 로그인_실패_이메일_필드_유효성() throws Exception {
+        // given
+        final LoginRequest request = LoginRequest.builder()
+                .email("invalid-email")
+                .password("user1-password")
+                .build();
+
+        // when
+        final ResultActions resultActions = requestLogin(request);
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("httpStatus").value(ErrorCode.INVALID_FIELDS.getHttpStatus().name()))
+                .andExpect(jsonPath("errorCode").value(ErrorCode.INVALID_FIELDS.getCode()))
+                .andExpect(jsonPath("message").value("이메일 형식에 맞지 않습니다."));
+    }
+
+    /**
+     * 사용자 로그인 실패
+     * - 실패 사유 : 비밀번호 필드 - null
+     */
+    @Test
+    public void 로그인_실패_비밀번호_필드_null() throws Exception {
+        // given
+        final LoginRequest request = LoginRequest.builder()
+                .email("user1@gmail.com")
+                .build();
+
+        // when
+        final ResultActions resultActions = requestLogin(request);
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("httpStatus").value(ErrorCode.INVALID_FIELDS.getHttpStatus().name()))
+                .andExpect(jsonPath("errorCode").value(ErrorCode.INVALID_FIELDS.getCode()))
+                .andExpect(jsonPath("message").value("비밀번호는 필수 입력값입니다."));
+    }
+
+    /**
+     * 사용자 로그인 실패
+     * - 실패 사유 : 비밀번호 필드 유효성
+     */
+    @Test
+    public void 로그인_실패_비밀번호_필드_유효성() throws Exception {
+        // given
+        final LoginRequest request = LoginRequest.builder()
+                .email("user1@gmail.com")
+                .password("invalid-password")
+                .build();
+
+        // when
+        final ResultActions resultActions = requestLogin(request);
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("httpStatus").value(ErrorCode.INVALID_FIELDS.getHttpStatus().name()))
+                .andExpect(jsonPath("errorCode").value(ErrorCode.INVALID_FIELDS.getCode()))
+                .andExpect(jsonPath("message").value("비밀번호는 8 자리 이상 영문, 숫자, 특수문자를 사용하세요."));
+    }
+
+    /**
+     * 사용자 로그인 실패
+     * - 실패 사유 : 요청 데이터 인증 실패
+     * - 존재하지 않는 이메일
+     * - 회원 정보와 일치하지 않는 비밀번호
+     */
+    @Test
+    public void 로그인_실패_인증정보() throws Exception {
+        // given
+        final LoginRequest request = LoginRequest.builder()
+                .email("invalid-user@gmail.com")
+                .password("invalid-user1-password")
+                .build();
+
+        // stub
+        when(userAuthService.login(any(), any())).thenThrow(new CustomCommonException(ErrorCode.INVALID_CREDENTIALS));
+
+        // when
+        final ResultActions resultActions = requestLogin(request);
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("httpStatus").value(ErrorCode.INVALID_CREDENTIALS.getHttpStatus().name()))
+                .andExpect(jsonPath("errorCode").value(ErrorCode.INVALID_CREDENTIALS.getCode()))
+                .andExpect(jsonPath("message").value(ErrorCode.INVALID_CREDENTIALS.getMessage()));
     }
 
     private ResultActions requestLogin(LoginRequest request) throws Exception {

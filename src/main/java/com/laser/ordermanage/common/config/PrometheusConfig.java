10de60d159ffc3dda0728f531f7fc19aa81b6915
yuseogi0218 <yuseogi0218@gmail.com>
2024-01-16 14:42:06 +0900

    #167 FEAT - Prometheus 설정

---
+++ src/main/java/com/laser/ordermanage/common/config/PrometheusConfig.java
@@ -0,0 +1,15 @@
+package com.laser.ordermanage.common.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrometheusConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer() {
+        return (registry) -> registry.config().commonTags("application", "ORDER-MANAGER");
+    }
+}

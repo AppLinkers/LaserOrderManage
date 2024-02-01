91f7329cc3d7aac626beb3e5bd0343ed1a2c5fc7
yuseogi0218 <yuseogi0218@gmail.com>
2024-02-01 09:02:09 +0900

    #215 TEST - UserEntity 단위 테스트 작성

---
+++ src/test/java/com/laser/ordermanage/user/unit/domain/UserEntityTest.java
@@ -0,0 +1,69 @@
+package com.laser.ordermanage.user.unit.domain;
+
+import com.laser.ordermanage.user.domain.UserEntity;
+import com.laser.ordermanage.user.domain.UserEntityBuilder;
+import com.laser.ordermanage.user.dto.request.UpdateUserAccountRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class UserEntityTest {
+
+    @Test
+    public void changePassword() {
+        // given
+        final UserEntity actualUser = UserEntityBuilder.build();
+        final String expectedPassword = "newPassword";
+
+        // when
+        actualUser.changePassword(expectedPassword);
+
+        // then
+        Assertions.assertThat(actualUser.getPassword()).isEqualTo(expectedPassword);
+    }
+
+    @Test
+    public void changeEmailNotification() {
+        // given
+        final UserEntity actualUser = UserEntityBuilder.build();
+        final Boolean expectedEmailNotification = Boolean.FALSE;
+
+        // when
+        actualUser.changeEmailNotification(expectedEmailNotification);
+
+        // then
+        Assertions.assertThat(actualUser.getEmailNotification()).isEqualTo(expectedEmailNotification);
+    }
+
+    @Test
+    public void updateProperties() {
+        // given
+        final UserEntity actualUser = UserEntityBuilder.build();
+        final UpdateUserAccountRequest expectedUserAccount = new UpdateUserAccountRequest("01011112222", "22222", "user1_address2", "user1_detail_address2");
+
+        // when
+        actualUser.updateProperties(expectedUserAccount);
+
+        // then
+        Assertions.assertThat(actualUser.getPhone()).isEqualTo(expectedUserAccount.phone());
+        Assertions.assertThat(actualUser.getAddress().getZipCode()).isEqualTo(expectedUserAccount.zipCode());
+        Assertions.assertThat(actualUser.getAddress().getAddress()).isEqualTo(expectedUserAccount.address());
+        Assertions.assertThat(actualUser.getAddress().getDetailAddress()).isEqualTo(expectedUserAccount.detailAddress());
+    }
+
+    @Test
+    public void extendsUserDetails() {
+        // given
+        final UserEntity actualUser = UserEntityBuilder.build();
+
+        // then
+        Assertions.assertThat(actualUser.getAuthorities().size()).isEqualTo(1);
+        Assertions.assertThat(actualUser.getAuthorities().toArray()).contains(new SimpleGrantedAuthority(actualUser.getRole().name()));
+
+        Assertions.assertThat(actualUser.getUsername()).isEqualTo(actualUser.getEmail());
+        Assertions.assertThat(actualUser.isAccountNonExpired()).isTrue();
+        Assertions.assertThat(actualUser.isAccountNonLocked()).isTrue();
+        Assertions.assertThat(actualUser.isCredentialsNonExpired()).isTrue();
+        Assertions.assertThat(actualUser.isEnabled()).isTrue();
+    }
+}

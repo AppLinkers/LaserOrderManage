d3d12ac41f16477bdabb67cd1ff8a8d3f2fa012b
yuseogi0218 <yuseogi0218@gmail.com>
2024-02-22 13:35:30 +0900

    #217 FIX - 자재 재고 현황 서비스 코드 수정

--- src/main/java/com/laser/ordermanage/ingredient/repository/IngredientPriceRepository.java
+++ src/main/java/com/laser/ordermanage/ingredient/repository/IngredientPriceRepository.java
@@ -3,6 +3,9 @@ package com.laser.ordermanage.ingredient.repository;
 import com.laser.ordermanage.ingredient.domain.IngredientPrice;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface IngredientPriceRepository extends CrudRepository<IngredientPrice, Long> {
-    IngredientPrice findFirstByIngredient_idOrderByCreatedAtDesc(Long ingredientId);
+    Optional<IngredientPrice> findByIngredientIdAndCreatedAt(Long ingredientId, LocalDate date);
 }
--- src/main/java/com/laser/ordermanage/ingredient/repository/IngredientStockRepository.java
+++ src/main/java/com/laser/ordermanage/ingredient/repository/IngredientStockRepository.java
@@ -3,8 +3,9 @@ package com.laser.ordermanage.ingredient.repository;
 import com.laser.ordermanage.ingredient.domain.IngredientStock;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
-public interface IngredientStockRepository extends CrudRepository<IngredientStock, Long> {
-    IngredientStock findFirstByIngredient_idOrderByCreatedAtDesc(Long ingredientId);
+public interface IngredientStockRepository extends CrudRepository<IngredientStock, Long>, IngredientStockRepositoryCustom {
+    Optional<IngredientStock> findByIngredientIdAndCreatedAt(Long ingredientId, LocalDate date);
 }

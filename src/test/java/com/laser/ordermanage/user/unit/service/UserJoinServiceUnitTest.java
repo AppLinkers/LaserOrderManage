888f893e8f3a0d552504c7130a24b2f190120804
yuseogi0218 <yuseogi0218@gmail.com>
2024-03-11 09:10:06 +0900

    #262 TEST - 이메일 인증 코드 생성 및 이메일 전송 기능 Service 단위 테스트 작성

--- src/test/java/com/laser/ordermanage/user/unit/service/UserJoinServiceUnitTest.java
+++ src/test/java/com/laser/ordermanage/user/unit/service/UserJoinServiceUnitTest.java
@@ -4,13 +4,21 @@ import com.laser.ordermanage.common.ServiceUnitTest;
 import com.laser.ordermanage.common.cache.redis.repository.VerifyCodeRedisRepository;
 import com.laser.ordermanage.common.email.EmailService;
 import com.laser.ordermanage.customer.repository.DeliveryAddressRepository;
+import com.laser.ordermanage.user.domain.UserEntity;
+import com.laser.ordermanage.user.domain.UserEntityBuilder;
 import com.laser.ordermanage.user.dto.response.UserJoinStatusResponse;
+import com.laser.ordermanage.user.dto.type.JoinStatus;
 import com.laser.ordermanage.user.repository.UserEntityRepository;
 import com.laser.ordermanage.user.service.UserJoinService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 public class UserJoinServiceUnitTest extends ServiceUnitTest {
 
@@ -27,23 +35,55 @@ public class UserJoinServiceUnitTest extends ServiceUnitTest {
     DeliveryAddressRepository deliveryAddressRepository;
 
     @Mock
-    UserEntityRepository userEntityRepository;
+    UserEntityRepository userRepository;
 
     @Mock
     VerifyCodeRedisRepository verifyCodeRedisRepository;
 
     /**
      * 이메일 인증코드 생성 및 이메일 전송 성공
+     * - 신규 회원
      */
     @Test
-    public void requestEmailVerify_성공() {
+    public void requestEmailVerify_성공_신규회원() throws Exception {
         // given
+        final UserJoinStatusResponse expectedResponse = UserJoinStatusResponse.builderWithOutUserEntity()
+                .status(JoinStatus.POSSIBLE)
+                .buildWithOutUserEntity();
         final String email = "new-user@gmail.com";
 
+        // stub
+        when(userRepository.findFirstByEmail(email)).thenReturn(Optional.empty());
+
         // when
-        final UserJoinStatusResponse userJoinStatusResponse = userJoinService.requestEmailVerify(email);
+        final UserJoinStatusResponse actualResponse = userJoinService.requestEmailVerify(email);
 
         // then
-
+        Assertions.assertThat(actualResponse).isEqualTo(expectedResponse);
     }
+
+    /**
+     * 이메일 인증코드 생성 및 이메일 전송 성공
+     * - 이메일 중복
+     */
+    @Test
+    public void requestEmailVerify_성공_이메일_중복() throws Exception {
+        // given
+        final UserEntity user = UserEntityBuilder.build();
+        final UserJoinStatusResponse expectedResponse = UserJoinStatusResponse.builderWithUserEntity()
+                .userEntity(user)
+                .status(JoinStatus.IMPOSSIBLE)
+                .buildWithUserEntity();
+
+        // stub
+        when(userRepository.findFirstByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        // when
+        final UserJoinStatusResponse actualResponse = userJoinService.requestEmailVerify(user.getEmail());
+
+        // then
+        Assertions.assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+
 }

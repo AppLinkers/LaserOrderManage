e55bb80646c9a6765daac6c7ea84a98747e00fc7
yuseogi0218 <yuseogi0218@gmail.com>
2024-04-02 15:09:09 +0900

    TEMP - 에러 로깅

--- src/main/java/com/laser/ordermanage/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laser/ordermanage/common/exception/GlobalExceptionHandler.java
@@ -49,7 +49,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUnknownException(Exception e, HttpServletRequest request) {
-        sendSlackAlertErrorLog(e, request); // 슬랙 알림 보내는 메서드
+        e.printStackTrace();
+//        sendSlackAlertErrorLog(e, request); // 슬랙 알림 보내는 메서드
         CustomCommonException exception = new CustomCommonException(CommonErrorCode.UNKNOWN_ERROR);
         return exception.toErrorResponse();
     }
